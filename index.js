da6097606002ebea430a8f0856ec6f5bf6ebb6b1
Rodrigo <rodrigo.facury14@gmail.com>
2022-09-05 16:06:37 -0300

    chore: testa entrega de informações do usuário

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const User = require('./database/models/user');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+});
+
+app.get('/users', async function (req, res) {
+  const users = User.findAll();
+  res.send(users);
 })
 
 app.listen(PORT, () => console.log(`ouvindo na porta ${PORT}`));
