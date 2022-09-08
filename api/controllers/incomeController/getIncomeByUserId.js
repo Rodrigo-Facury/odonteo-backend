f04cc002b110adeab8eb8c6c2d259a649ff6ce6f
Rodrigo <rodrigo.facury14@gmail.com>
2022-09-08 12:07:30 -0300

    feat: inicia lógica para obter registro de renda por id de usuário

---
+++ api/controllers/incomeController/getIncomeByUserId.js
@@ -0,0 +1,21 @@
+const { Income: incomeModule } = require('../../../database/models');
+
+async function getIncomeByUserId(req, res, next) {
+  try {
+    const { params: { userId } } = req;
+
+    const income = await incomeModule.findAll({
+      where: {
+        userId
+      },
+      attributes: ['dates', 'installmentValue']
+    });;
+
+    return res.status(200).json({ income });
+
+  } catch(err) {
+    return next(err);
+  }
+}
+
+module.exports = getIncomeByUserId;
--- api/routes/incomeRoutes.js
+++ api/routes/incomeRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const getIncomeByUserId = require('../controllers/incomeController/getIncomeByUserId');
 const registerIncome = require('../controllers/incomeController/registerIncome');
 
 const router = express.Router();
 
 router.post('/', registerIncome);
+
+router.get('/:userId', getIncomeByUserId);
 
 module.exports = router;
